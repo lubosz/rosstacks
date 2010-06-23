lubosz/rosstacks

commit 03e50f2b04d2888f856d5a8ad082df7d34bcc082

The main purpose of this checkin is to fix a deadlock in roscpp (ticket 4195 in ros-pkg) when the connection has already read all the data from a service response but the ServiceServerLink has not actually asked for the response data yet.
The fix for this both:
 1) Removed an extra copy happening in the Connection class, where we were reading data into a fixed-size 65k receive buffer before copying data into the read buffer (this was an optimization, but it turns out that due to other decisions it shouldn't actually help).
 2) Exposed a bug in the UDP implementation, where it assumed that the # of writes on one end always matched the # of reads on the other.

The majority of this change in terms of how dangerous it is is due to #2 -- I believe I know exactly what was going wrong, and that my fix is correct, but I may have introduced subtle bugs.  All tests pass on my 4-core Karmic machine.



git-svn-id: https://code.ros.org/svn/ros/stacks/ros/trunk@10136 61973afe-1cd6-434e-a0a9-934cb0052259

core/roscpp/src/libros/connection.cpp
@@ -47,7 +47,6 @@ namespace ros
 Connection::Connection()
 : is_server_(false)
 , dropped_(false)
-, fixed_read_filled_(0)
 , read_filled_(0)
 , read_size_(0)
 , reading_(false)
@@ -115,56 +114,41 @@ void Connection::readTransport()
 
   reading_ = true;
 
-  if (fixed_read_filled_ < READ_BUFFER_SIZE)
+  while (!dropped_ && has_read_callback_)
   {
-    int32_t bytes_read = transport_->read(fixed_read_buffer_ + fixed_read_filled_, READ_BUFFER_SIZE - fixed_read_filled_);
-    ROS_DEBUG_NAMED("superdebug", "Connection read %d bytes", bytes_read);
-    if (dropped_)
+    ROS_ASSERT(read_buffer_);
+    uint32_t to_read = read_size_ - read_filled_;
+    if (to_read > 0)
     {
-      return;
+      int32_t bytes_read = transport_->read(read_buffer_.get() + read_filled_, to_read);
+      ROS_DEBUG_NAMED("superdebug", "Connection read %d bytes", bytes_read);
+      if (dropped_)
+      {
+        return;
+      }
+      else if (bytes_read < 0)
+      {
+        // Bad read, throw away results and report error
+        ReadFinishedFunc callback;
+        callback = read_callback_;
+        read_callback_.clear();
+        read_buffer_.reset();
+        uint32_t size = read_size_;
+        read_size_ = 0;
+        read_filled_ = 0;
+        has_read_callback_ = 0;
+
+        if (callback)
+        {
+          callback(shared_from_this(), read_buffer_, size, false);
+        }
+      }
+
+      read_filled_ += bytes_read;
     }
-    else if (bytes_read < 0)
-    {
-      reading_ = false;
 
-      // Bad read, throw away results and report error
-      ReadFinishedFunc callback;
-      callback = read_callback_;
-      read_callback_ = ReadFinishedFunc();
-      read_buffer_ = boost::shared_array<uint8_t>();
-      uint32_t size = read_size_;
-      read_size_ = 0;
-      read_filled_ = 0;
-      has_read_callback_ = 0;
-      fixed_read_filled_ = 0;
-
-      if (callback)
-        callback(shared_from_this(), read_buffer_, size, false);
-      return;
-    }
-
-    fixed_read_filled_ += bytes_read;
-  }
-  else
-  {
-    if (has_read_callback_)
-    {
-      ROS_WARN("Connection read buffer filled with no read callback set");
-    }
-  }
-
-  while (has_read_callback_ && (fixed_read_filled_ > 0 || read_size_ == 0) && !dropped_)
-  {
     ROS_ASSERT((int)read_size_ >= 0);
     ROS_ASSERT((int)read_filled_ >= 0);
-
-    uint32_t write_amount = std::min(read_size_ - read_filled_, fixed_read_filled_);
-    ROS_DEBUG_NAMED("superdebug", "Copying %d bytes into read buffer", write_amount);
-    memcpy(read_buffer_.get() + read_filled_, fixed_read_buffer_, write_amount);
-    memmove(fixed_read_buffer_, fixed_read_buffer_ + write_amount, fixed_read_filled_ - write_amount);
-    fixed_read_filled_ -= write_amount;
-    read_filled_ += write_amount;
-
     ROS_ASSERT(read_filled_ <= read_size_);
 
     if (read_filled_ == read_size_ && !dropped_)
@@ -179,8 +163,8 @@ void Connection::readTransport()
       callback = read_callback_;
       size = read_size_;
       buffer = read_buffer_;
-      read_callback_ = ReadFinishedFunc();
-      read_buffer_ = boost::shared_array<uint8_t>();
+      read_callback_.clear();
+      read_buffer_.reset();
       read_size_ = 0;
       read_filled_ = 0;
       has_read_callback_ = 0;
@@ -188,6 +172,15 @@ void Connection::readTransport()
       ROS_DEBUG_NAMED("superdebug", "Calling read callback");
       callback(shared_from_this(), buffer, size, true);
     }
+    else
+    {
+      break;
+    }
+  }
+
+  if (!has_read_callback_)
+  {
+    transport_->disableRead();
   }
 
   reading_ = false;
@@ -282,6 +275,8 @@ void Connection::read(uint32_t size, const ReadFinishedFunc& callback)
     read_filled_ = 0;
     has_read_callback_ = 1;
   }
+
+  transport_->enableRead();
 
   // read immediately if possible
   readTransport();

core/roscpp/include/ros/connection.h
@@ -215,13 +215,6 @@ private:
   /// Function that handles the incoming header
   HeaderReceivedFunc header_func_;
 
-  /**
-   * If there is data available to read, we always try to read into our fixed read buffer, even if a read request
-   * has not been made.
-   */
-  uint8_t fixed_read_buffer_[READ_BUFFER_SIZE];
-  uint32_t fixed_read_filled_;
-
   /// Read buffer that ends up being passed to the read callback
   boost::shared_array<uint8_t> read_buffer_;
   /// Amount of data currently in the read buffer, in bytes

core/roscpp/include/ros/transport/transport.h
@@ -82,6 +82,15 @@ public:
   virtual void disableWrite() = 0;
 
   /**
+   * \brief Enable reading on this transport.  Allows derived classes to, for example, enable read polling for asynchronous sockets
+   */
+  virtual void enableRead() = 0;
+  /**
+   * \brief Disable reading on this transport.  Allows derived classes to, for example, disable read polling for asynchronous sockets
+   */
+  virtual void disableRead() = 0;
+
+  /**
    * \brief Close this transport.  Once this call has returned, writing on this transport should always return an error.
    */
   virtual void close() = 0;

core/roscpp/include/ros/transport/transport_tcp.h
@@ -109,6 +109,8 @@ public:
 
   virtual void enableWrite();
   virtual void disableWrite();
+  virtual void enableRead();
+  virtual void disableRead();
 
   virtual void close();
 
@@ -132,11 +134,6 @@ private:
    * \return Whether setting the socket was successful
    */
   bool setSocket(int sock);
-
-  /**
-   * \brief Enables reading on our socket
-   */
-  void enableRead();
 
   void socketUpdate(int events);
 

core/roscpp/include/ros/transport/transport_udp.h
@@ -39,7 +39,6 @@
 #include <ros/transport/transport.h>
 
 #include <boost/thread/mutex.hpp>
-#include <boost/random.hpp>
 
 #include <netinet/in.h>
 
@@ -103,17 +102,14 @@ public:
    */
   int getServerPort() const {return server_port_;}
 
-  /**
-   * \brief Get a unique connection ID
-   */
-  int generateConnectionId() {return gen_();}
-
   // overrides from Transport
   virtual int32_t read(uint8_t* buffer, uint32_t size);
   virtual int32_t write(uint8_t* buffer, uint32_t size);
 
   virtual void enableWrite();
   virtual void disableWrite();
+  virtual void enableRead();
+  virtual void disableRead();
 
   virtual void close();
 
@@ -138,11 +134,6 @@ private:
    */
   bool setSocket(int sock);
 
-  /**
-   * \brief Enables reading on our socket
-   */
-  void enableRead();
-
   void socketUpdate(int events);
 
   int sock_;
@@ -161,14 +152,16 @@ private:
   PollSet* poll_set_;
   int flags_;
 
-  boost::rand48 gen_;
-
   uint32_t connection_id_;
   uint8_t current_message_id_;
   uint16_t total_blocks_;
   uint16_t last_block_;
 
   uint32_t max_datagram_size_;
+
+  uint8_t* data_buffer_;
+  uint8_t* data_start_;
+  uint32_t data_filled_;
 
   uint8_t *reorder_buffer_;
   TransportUDPHeader reorder_header_;

core/roscpp/src/libros/service_server_link.cpp
@@ -177,6 +177,7 @@ void ServiceServerLink::onConnectionDropped(const ConnectionPtr& conn)
 
 void ServiceServerLink::onRequestWritten(const ConnectionPtr& conn)
 {
+  //ros::WallDuration(0.1).sleep();
   connection_->read(5, boost::bind(&ServiceServerLink::onResponseOkAndLength, this, _1, _2, _3, _4));
 }
 
@@ -321,6 +322,8 @@ bool ServiceServerLink::call(const SerializedMessage& req, SerializedMessage& re
   info->finished_ = false;
   info->call_finished_ = false;
   info->caller_thread_id_ = boost::this_thread::get_id();
+
+  //ros::WallDuration(0.1).sleep();
 
   bool immediate = false;
   {

core/roscpp/src/libros/topic_manager.cpp
@@ -648,7 +648,7 @@ bool TopicManager::requestTopic(const string &topic,
       }
 
       int max_datagram_size = proto[4];
-      int conn_id = connection_manager_->getUDPServerTransport()->generateConnectionId();
+      int conn_id = connection_manager_->getNewConnectionID();
       TransportUDPPtr transport = connection_manager_->getUDPServerTransport()->createOutgoing(host, port, conn_id, max_datagram_size);
       connection_manager_->udprosIncomingConnection(transport, h);
 

core/roscpp/src/libros/transport/transport_tcp.cpp
@@ -132,7 +132,7 @@ bool TransportTCP::initializeSocket()
 
   if (!(flags_ & SYNCHRONOUS))
   {
-    enableRead();
+    //enableRead();
   }
 
   return true;
@@ -314,6 +314,11 @@ bool TransportTCP::listen(int port, int backlog, const AcceptCallback& accept_cb
     return false;
   }
 
+  if (!(flags_ & SYNCHRONOUS))
+  {
+    enableRead();
+  }
+
   return true;
 }
 
@@ -447,6 +452,26 @@ void TransportTCP::enableRead()
   {
     poll_set_->addEvents(sock_, POLLIN);
     expecting_read_ = true;
+  }
+}
+
+void TransportTCP::disableRead()
+{
+  ROS_ASSERT(!(flags_ & SYNCHRONOUS));
+
+  {
+    boost::recursive_mutex::scoped_lock lock(close_mutex_);
+
+    if (closed_)
+    {
+      return;
+    }
+  }
+
+  if (expecting_read_)
+  {
+    poll_set_->delEvents(sock_, POLLIN);
+    expecting_read_ = false;
   }
 }
 

commit 8f234d27e862ff50dcb3ae70c4a3ce2a78e19538

fix assertion in Connection caused by r10136
git-svn-id: https://code.ros.org/svn/ros/stacks/ros/trunk@10139 61973afe-1cd6-434e-a0a9-934cb0052259

core/roscpp/src/libros/connection.cpp
@@ -142,6 +142,8 @@ void Connection::readTransport()
         {
           callback(shared_from_this(), read_buffer_, size, false);
         }
+
+        break;
       }
 
       read_filled_ += bytes_read;